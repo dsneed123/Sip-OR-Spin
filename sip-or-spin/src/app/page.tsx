872f2c9bbae27b4a180644d50e86aae522e74f9c
Davis Sneed <dlsneed1298@gmail.com>
2025-03-02 19:30:16 -0800

    rules

--- sip-or-spin/src/app/page.tsx
+++ sip-or-spin/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           Sip or Spin is the ultimate game to ensure a fun-filled night with your friends. Whether you&lsquo;re looking to explore new drinks or take a chance with the spin wheel, you&lsquo;re guaranteed to have a memorable time!
         </p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <Link className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-[#383838] text-white hover:bg-[#555] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" href="/rules">
+            <Link className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-[#383838] text-white hover:bg-[#555] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" href="/Rules">
             Rules
             </Link>
             <Link className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center bg-[#f2f2f2] text-black hover:bg-[#e0e0e0] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44" href="/CreateGame">
