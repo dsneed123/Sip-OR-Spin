024209558925bb5674ab08030638ce7e8542c6d7
Davis Sneed <dlsneed1298@gmail.com>
2025-03-03 13:33:47 -0800

    THIS MIGHT WORK LOL

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import CryptoJS from "crypto-js";
 import Spinner from "../components/Spinner";
 
