40b67a702b384386508a475af9d465f9691fd481
Davis Sneed <dlsneed1298@gmail.com>
2025-03-03 13:41:18 -0800

    i do not care that window is not defined. CHILL OUT ESLINT

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@
 const nextConfig = {
   basePath: '/Sip-OR-Spin', // Replace with your repo name
   assetPrefix: '/Sip-OR-Spin', // Replace with your repo name
-
+  utput: "export",
 };
 
 export default nextConfig;
