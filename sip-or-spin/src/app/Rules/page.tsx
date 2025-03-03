ad3ceda761d117eeecbda97ed7f4a601a65c0d7c
lbodenbender <lbodenbender@zagmail.gonzaga.edu>
2025-03-03 13:30:22 -0800

    flexbox

--- sip-or-spin/src/app/Rules/page.tsx
+++ sip-or-spin/src/app/Rules/page.tsx
@@ -2,9 +2,16 @@ import React from "react";
 const rules = () => {
     return (
         <>
-        {/* TODO Lindsey: design a rule page here */}
-        <p>rules</p>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={{ flex: 1, backgroundColor: 'black' }}>
+                    <p style= {{ flex: 2, color: 'white'}}>div1</p>
+                </div>
+                <div style= {{ backgroundColor: 'pink' }}>
+                    <p>div2</p>
+                </div>
+            </div>
         </>
     );
 }
+
 export default rules;
