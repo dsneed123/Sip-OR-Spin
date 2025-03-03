78e46c94cbd31cf8ff8bf60140215297fe50dbe7
lbodenbender <lbodenbender@zagmail.gonzaga.edu>
2025-03-03 13:52:31 -0800

    align items

--- sip-or-spin/src/app/Rules/page.tsx
+++ sip-or-spin/src/app/Rules/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 const rules = () => {
     return (
         <>
+            <div>
+                <h1 style={{ fontSize: '50px', textAlign: 'center' }}>Rules</h1>
+            </div>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <div style={{ flex: 1, backgroundColor: 'black' }}>
-                    <p style= {{ flex: 2, color: 'white'}}>div1</p>
+                <div style={{ alignItems: 'stretch', flex: 1, backgroundColor: 'black' }}>
+                    <p style= {{ color: 'white' }}>div1</p>
                 </div>
-                <div style= {{ backgroundColor: 'pink' }}>
+                <div style= {{ alignItems: 'stretch', flex: 2, backgroundColor: 'pink' }}>
                     <p>div2</p>
                 </div>
             </div>
