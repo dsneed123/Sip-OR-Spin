df445e2f768c0b518f8e7de0a0cb90e0041cda91
lbodenbender <lbodenbender@zagmail.gonzaga.edu>
2025-03-03 14:51:59 -0800

    set width

--- sip-or-spin/src/app/Rules/page.tsx
+++ sip-or-spin/src/app/Rules/page.tsx
@@ -6,10 +6,13 @@ const rules = () => {
                 <h1 className="pt-5 pb-10 text-5xl text-center">Rules</h1>
             </div>
             <div style={{ display: 'flex', gap: '40px', flexDirection: 'column', alignItems: 'center'}}>
-                <div className="flex-1 bg-[#FFECDC] rounded-3xl pt-50 pb-50 pr-150 pl-150">
+                <div className="flex-1 bg-[#FFECDC] rounded-3xl w-2/3">
+                    <div style= {{ alignSelf: 'flex-start', zIndex: 1, top: 0}}>
+                        <h2 style= {{ whiteSpace: 'nowrap' }}>How to Play</h2>
+                    </div>
                     <p>div1</p>
                 </div>
-                <div className="flex-1 bg-[#FFECDC] rounded-3xl pt-50 pb-50 pr-150 pl-150">
+                <div className="flex-1 bg-[#FFECDC] rounded-3xl w-2/3">
                     <p>div2</p>
                 </div>
             </div>
