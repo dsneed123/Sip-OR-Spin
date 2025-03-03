b70cad6ee516048bae0dd54a0a7047cf2d06f1e4
Davis Sneed <dlsneed1298@gmail.com>
2025-03-03 12:01:32 -0800

    config

--- sip-or-spin/next.config.ts
+++ sip-or-spin/next.config.ts
@@ -1,9 +1,9 @@
 // import type { NextConfig } from "next";
 
 const nextConfig = {
-  // basePath: '/Sip-OR-Spin/', // Replace with your repo name
-  // assetPrefix: '/Sip-OR-Spin/', // Replace with your repo name
-  // output: 'export',
+  basePath: '/Sip-OR-Spin', // Replace with your repo name
+  assetPrefix: '/Sip-OR-Spin/', // Replace with your repo name
+  output: 'export',
 };
 
 export default nextConfig;
