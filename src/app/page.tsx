22fbc5d483f26d8d9a2efb07570f8c204c53e5a0
Davis Sneed <dlsneed1298@gmail.com>
2025-03-03 19:36:53 -0800

    fixed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center bg-black justify-items-center min-h-screen p-8 pb-20 "
       style={{ fontFamily: "var(--font-geist-sans)" }}>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-black text-white pt-16 sm:pt-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-black text-white ">
       <div className="mt-5 flex flex-col items-center sm:items-start">
         <img src="../../logo.png" alt="Sip or Spin logo" width={350} height={38} className="mb-4" />
         <h1 className="text-4xl font-bold text-center sm:text-left">
