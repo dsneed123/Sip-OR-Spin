e353700ea881c223836e7cc610b4b8bc0dd170c0
Davis Sneed <dlsneed1298@gmail.com>
2025-03-03 18:08:35 -0800

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Spin or Sip</title>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
